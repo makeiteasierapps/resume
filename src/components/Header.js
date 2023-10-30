5b1146f5c2c3dee7eb2b88e0902ad3f01a120685
Shaun Offenbacher <shaunoffenbacher@yahoo.com>
2023-10-30 12:53:52 -0600

    more mobile responsive layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,79 +1,78 @@
-import { useContext } from "react";
-import { Navbar, Nav, NavItem, NavLink } from "reactstrap";
-import { ThemeContext } from "../contexts/ThemeContext";
+import { useContext } from 'react';
+import { Navbar, Nav, NavItem, NavLink } from 'reactstrap';
+import { ThemeContext } from '../contexts/ThemeContext';
 const Header = () => {
-  const theme = useContext(ThemeContext);
-  return (
-    <Navbar
-      expand="xs"
-      fixed="top"
-      style={{ backgroundColor: theme.deepPurple }}
-    >
-      <Nav
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          width: "100%",
-          fontFamily: theme.mainText,
-          color: theme.lightBlue,
-          fontSize: '1rem'
-        }}
-        navbar
-      >
-        <NavItem>
-          <NavLink
-            href="#home"
-            style={{
-              color: theme.lightBlue,
-            }}
-          >
-            HOME
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            href="#education"
-            style={{
-              color: theme.lightBlue,
-              
-            }}
-          >
-            EDUCATION
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            href="#skills"
-            style={{
-              color: theme.lightBlue,
-            }}
-          >
-            SKILLS
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            href="#portfolio"
-            style={{
-              color: theme.lightBlue,
-            }}
-          >
-            PORTFOLIO
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            href="#contact"
-            style={{
-              color: theme.lightBlue,
-            }}
-          >
-            CONTACT
-          </NavLink>
-        </NavItem>
-      </Nav>
-    </Navbar>
-  );
+    const theme = useContext(ThemeContext);
+    return (
+        <Navbar
+            expand="xs"
+            fixed="top"
+            style={{ backgroundColor: theme.deepPurple }}
+        >
+            <Nav
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    width: '100%',
+                    fontFamily: theme.mainText,
+                    color: theme.lightBlue,
+                    fontSize: 'clamp(10px, 3vw, 20px)', // change this line
+                }}
+                navbar
+            >
+                <NavItem>
+                    <NavLink
+                        href="#home"
+                        style={{
+                            color: theme.lightBlue,
+                        }}
+                    >
+                        HOME
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+                        href="#education"
+                        style={{
+                            color: theme.lightBlue,
+                        }}
+                    >
+                        EDUCATION
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+                        href="#skills"
+                        style={{
+                            color: theme.lightBlue,
+                        }}
+                    >
+                        SKILLS
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+                        href="#portfolio"
+                        style={{
+                            color: theme.lightBlue,
+                        }}
+                    >
+                        PORTFOLIO
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+                        href="#contact"
+                        style={{
+                            color: theme.lightBlue,
+                        }}
+                    >
+                        CONTACT
+                    </NavLink>
+                </NavItem>
+            </Nav>
+        </Navbar>
+    );
 };
 
 export default Header;
