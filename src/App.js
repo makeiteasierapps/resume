7ae2fc8b094f5b17e5acd641a63dd22c03ca2084
Shaun Offenbacher <makeiteasierapps@gmail.com>
2023-11-02 11:12:03 -0600

    more responsiveness

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useRef, useEffect } from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
 import NavBar from './components/Header';
 import Skills from './components/Skills';
@@ -7,67 +6,14 @@ import Portfolio from './components/Portfolio';
 import ContactMe from './components/ContactMe';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import { Element, scroller } from 'react-scroll';
+import { Element } from 'react-scroll';
 import './styles/App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { debounce } from 'lodash';
 
 const App = () => {
-    const currentSectionIndex = useRef(0);
-    let lastScrollTop = useRef(0);
-    let isScrolling = useRef(false);
-
-    // useEffect(() => {
-    //     const handleScroll = debounce((e) => {
-    //         if (isScrolling.current) {
-    //             return;
-    //         }
-
-    //         const sections = [
-    //             'home',
-    //             'education',
-    //             'skills',
-    //             'portfolio',
-    //             'contact',
-    //         ];
-
-    //         const st = window.scrollY;
-    //         if (st > lastScrollTop.current) {
-    //             // Downscroll
-    //             if (currentSectionIndex.current < sections.length - 1) {
-    //                 currentSectionIndex.current =
-    //                     currentSectionIndex.current + 1;
-    //             }
-    //         } else {
-    //             // Upscroll
-    //             if (currentSectionIndex.current > 0) {
-    //                 currentSectionIndex.current =
-    //                     currentSectionIndex.current - 1;
-    //             }
-    //         }
-    //         lastScrollTop.current = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-
-    //         isScrolling.current = true;
-    //         scroller.scrollTo(sections[currentSectionIndex.current], {
-    //             duration: 500,
-    //             delay: 0,
-    //             smooth: 'easeInOutQuart',
-    //         });
-
-    //     }, 500); // Debounce time
-
-    //     window.addEventListener('scroll', handleScroll);
-
-    //     return () => window.removeEventListener('scroll', handleScroll);
-    // }, []);
-
     return (
         <ThemeContext.Provider value={ThemeContext._currentValue}>
-            <div
-                style={{
-                    height: '100vh',
-                }}
-            >
+            <div className="content-container">
                 <NavBar />
                 <Element name="home">
                     <Home />
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { Container, Col } from 'reactstrap';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { CardImg } from 'reactstrap';
-import { useSpring, animated, config } from 'react-spring';
+import { useSpring, animated } from 'react-spring';
 import '../styles/Home.scss';
 import shaunoProfile from '../assets/shauno_profile.png';
 
@@ -12,8 +12,8 @@ const Home = () => {
     const [ref, inView] = useInView();
     const theme = useContext(ThemeContext);
     const props = useSpring({
-        from: { opacity: 0, transform: 'translate3d(0,40px,0)' },
-        to: { opacity: inView ? 1 : 0, transform: 'translate3d(0,0px,0)' },
+        from: { opacity: 0, height: '0%' },
+        to: { opacity: inView ? 1 : 0, height: inView ? '100%' : '0%' },
         config: { tension: 210, friction: 20 },
     });
 
@@ -23,7 +23,7 @@ const Home = () => {
         flexDirection: 'column',
         justifyContent: 'space-evenly',
         width: '100%',
-        height: '100vh',
+        minHeight: '100vh',
         fontFamily: theme.mainText,
         color: 'black',
     };
@@ -31,7 +31,7 @@ const Home = () => {
     return (
         <Container
             id="home"
-            className="text-center mt-3"
+            className="text-center mt-3 section"
             style={pageStyle}
             fluid
         >
@@ -40,60 +40,55 @@ const Home = () => {
                 style={props}
                 className="d-flex align-items-center justify-content-center mt-md-3"
             >
-                <Col xs="12" md="9">
-                    <Container className="d-flex flex-column-reverse flex-md-row justify-content-center align-items-center">
-                        <Col xs="12" md="10">
-                            <Container className="text-wrap">
-                                <Container
-                                    className="mt-2 fs-1 text-wrap"
-                                    style={{
-                                        fontWeight: '700',
-                                    }}
-                                >
-                                    Shaun Offenbacher
-                                </Container>
-                                <Container
-                                    className="mb-3 fs-5 text-wrap"
-                                    style={{
-                                        fontWeight: '500',
-                                        fontStyle: 'italic',
-                                    }}
-                                >
-                                    Software Engineer
-                                </Container>
-                                <Container
-                                    className="responsive-text"
-                                    style={{
-                                        fontWeight: '500',
-                                    }}
-                                >
-                                    Transitioning from fine dining to web
-                                    development, I bring a unique blend of
-                                    creativity, precision, and customer focus to
-                                    software engineering. My hospitality
-                                    background has honed my attention to detail
-                                    and commitment to exceptional service, which
-                                    I now apply to creating efficient code and
-                                    memorable digital experiences. I strive to
-                                    build innovative, user-friendly websites
-                                    that are visually appealing and leave a
-                                    lasting impression.
-                                </Container>
-                            </Container>
-                        </Col>
-                        <Col xs="12" md="4" className="align-self-center">
-                            <CardImg
-                                style={{
-                                    height: 'auto',
-                                    width: 'clamp(9.375rem, 23vw, 15.625rem)',
-                                }}
-                                src={shaunoProfile}
-                                alt="my avatar"
-                                className="rounded-circle img-fluid"
-                            />
-                        </Col>
-                    </Container>
-                </Col>
+                <Container className="d-flex flex-column-reverse flex-md-row justify-content-center align-items-center">
+                    <Col xs="12" md="">
+                        <Container
+                            className="mt-2 fs-1"
+                            style={{
+                                fontWeight: '700',
+                            }}
+                        >
+                            Shaun Offenbacher
+                        </Container>
+                        <Container
+                            className="mb-3 fs-5"
+                            style={{
+                                fontWeight: '500',
+                                fontStyle: 'italic',
+                            }}
+                        >
+                            Software Engineer
+                        </Container>
+                        <Container
+                            className="responsive-text"
+                            style={{
+                                fontWeight: '500',
+                            }}
+                        >
+                            Transitioning from fine dining to web development, I
+                            bring a unique blend of creativity, precision, and
+                            customer focus to software engineering. My
+                            hospitality background has honed my attention to
+                            detail and commitment to exceptional service, which
+                            I now apply to creating efficient code and memorable
+                            digital experiences. I strive to build innovative,
+                            user-friendly websites that are visually appealing
+                            and leave a lasting impression.
+                        </Container>
+                    </Col>
+                    <Col xs="12" md="4" className="align-self-center">
+                        <CardImg
+                            style={{
+                                height: 'auto',
+                                width: 'clamp(9.375rem, 23vw, 15.625rem)',
+                                paddingTop: '10px',
+                            }}
+                            src={shaunoProfile}
+                            alt="my avatar"
+                            className="rounded-circle img-fluid"
+                        />
+                    </Col>
+                </Container>
             </animated.div>
         </Container>
     );
