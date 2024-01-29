e793d02c06d23d3ce3915ec24aa87fce0c5bd48a
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-01-28 22:53:11 -0700

    Mored Project work

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,11 +3,8 @@ import { ThemeContext } from '../contexts/ThemeContext';
 import { Container, Row, Col } from 'reactstrap';
 import Project from './Project';
 
-import EmailManagerLogo from '../assets/email-manager-logo.jpeg';
-import EmailManagerLogin from '../assets/emailManagerLogin.png';
 const Portfolio = () => {
     const theme = useContext(ThemeContext);
-    const images = [EmailManagerLogo, EmailManagerLogin];
     return (
         <Container
             id="portfolio"
@@ -21,13 +18,7 @@ const Portfolio = () => {
         >
             <Row>
                 <Col>
-                    {/* <Project
-                        title="Email Manager"
-                        description="Full Stack Web App utilizing Mailgun and OpenAi API's. Built with React and Node"
-                        images={images}
-                        githubUrl="https://github.com/user/project1"
-                        hostedUrl="https://project1.com"
-                    /> */}
+                    <Project />
                 </Col>
             </Row>
         </Container>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,109 +1,133 @@
-import { useState } from 'react';
-import {
-    Card,
-    CardText,
-    CardBody,
-    CardTitle,
-    Button,
-    Carousel,
-    CarouselItem,
-    CarouselControl,
-    CarouselIndicators,
-    CarouselCaption,
-} from 'reactstrap';
+import { useState, useContext } from 'react';
+import { Row, Col, Container } from 'reactstrap';
+import { faReact } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Carousel from 'react-spring-3d-carousel';
+import { ThemeContext } from '../contexts/ThemeContext';
 
-const Project = ({
-    title,
-    description,
-    images,
-    videoUrl = null,
-    githubUrl,
-    hostedUrl,
-}) => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [animating, setAnimating] = useState(false);
+import EmailManagerLogo from '../assets/email-manager-logo.jpeg';
+import EmailManagerLogin from '../assets/emailManagerLogin.png';
+import { ReactComponent as OpenAiLogo } from '../assets/projectIcons/openai-lockup.svg';
+import { ReactComponent as MailgunLogo } from '../assets/projectIcons/mailgun.svg';
+import { ReactComponent as NodeLogo } from '../assets/skillsIcons/node-js.svg';
+import { ReactComponent as FirebaeLogo } from '../assets/projectIcons/firebase.svg';
+import MaterialUiLogo from '../assets/projectIcons/materialUi.png';
+import ReactLogo from '../assets/projectIcons/react.png';
 
-    const next = () => {
-        if (animating) return;
-        const nextIndex =
-            activeIndex === images.length - 1 ? 0 : activeIndex + 1;
-        setActiveIndex(nextIndex);
-    };
+const Project = () => {
+    const theme = useContext(ThemeContext);
+    const [slideIndex, setSlideIndex] = useState(0);
+    const images = [
+        {
+            image: EmailManagerLogo,
+            description:
+                "Full Stack Web App utilizing Mailgun and OpenAi API's. Built with React and Node",
+        },
+        {
+            image: EmailManagerLogin,
+            description:
+                'Login page for Email Manager. Built with React and Material UI',
+        },
+        {
+            image: EmailManagerLogin,
+            description:
+                'Login page for Email Manager. Built with React and Material UI',
+        },
+    ];
 
-    const previous = () => {
-        if (animating) return;
-        const nextIndex =
-            activeIndex === 0 ? images.length - 1 : activeIndex - 1;
-        setActiveIndex(nextIndex);
-    };
-
-    const goToIndex = (newIndex) => {
-        if (animating) return;
-        setActiveIndex(newIndex);
-    };
-
-    const slides = images.map((image, index) => {
-        return (
-            <CarouselItem
-                onExiting={() => setAnimating(true)}
-                onExited={() => setAnimating(false)}
-                key={index}
-            >
-                <img
-                    src={image}
-                    alt={title}
-                    style={{
-                        width: '100%',
-                        height: '500px',
-                        objectFit: 'cover',
-                    }}
-                />
-                <CarouselCaption
-                    captionText={description}
-                    captionHeader={title}
-                />
-            </CarouselItem>
-        );
-    });
+    const slides = images.map((image, index) => ({
+        key: index,
+        content: (
+            <img
+                src={image.image}
+                alt={'Email Manager'}
+                style={{
+                    backgroundColor: 'transparent',
+                    width: '20vw',
+                    height: '30vh',
+                }}
+            />
+        ),
+    }));
 
     return (
-        <Card>
-            <Carousel
-                activeIndex={activeIndex}
-                next={next}
-                previous={previous}
-                ride={null}
-                style={{ width: '70vw', height: '500px' }}
-            >
-                <CarouselIndicators
-                    items={images}
-                    activeIndex={activeIndex}
-                    onClickHandler={goToIndex}
-                />
-                {slides}
-                <CarouselControl
-                    direction="prev"
-                    directionText="Previous"
-                    onClickHandler={previous}
-                />
-                <CarouselControl
-                    direction="next"
-                    directionText="Next"
-                    onClickHandler={next}
-                />
-            </Carousel>
-            {videoUrl && (
-                <video width="320" height="240" controls>
-                    <source src={videoUrl} type="video/mp4" />
-                </video>
-            )}
-            <CardBody>
-                <CardTitle tag="h5">{title}</CardTitle>
-                <CardText>{description}</CardText>
-                <Button href={githubUrl}>Github</Button>
-                {hostedUrl && <Button href={hostedUrl}>Live Demo</Button>}
-            </CardBody>
-        </Card>
+        <Container
+            style={{
+                height: '50vh',
+                width: '70vw',
+                backgroundColor: 'transparent',
+                boxShadow: `0 0 13px   ${theme.darkTeal}`,
+                borderRadius: '10px',
+            }}
+        >
+            <Row style={{ height: '100%' }}>
+                <Col md="6" style={{ padding: '2rem' }}>
+                    <Row>
+                        <Col>
+                            <h1>Email Manager</h1>
+                            <p>
+                                Full Stack Web App utilizing Mailgun and OpenAi
+                                API's. Built with React and Node
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row style={{ paddingTop: '1rem' }}>
+                        <Col>
+                            <img
+                                src={ReactLogo}
+                                alt="React"
+                                style={{ width: '8rem' }}
+                            />
+                            <img
+                                src={MaterialUiLogo}
+                                alt="Material UI"
+                                style={{ width: '8rem' }}
+                            />
+                        </Col>
+                        <Col className="d-flex flex-column align-items-center">
+                            <Row>
+                                <NodeLogo
+                                    style={{
+                                        width: '8rem',
+                                        paddingBottom: '1.4rem',
+                                    }}
+                                />
+                            </Row>
+                            <Row>
+                                <MailgunLogo
+                                    style={{
+                                        width: '6rem',
+                                        paddingBottom: '1rem',
+                                        fill: '#fff',
+                                    }}
+                                />
+                            </Row>
+                            <Row>
+                                <OpenAiLogo
+                                    style={{
+                                        width: '6rem',
+                                        paddingBottom: '1rem',
+                                        fill: '#fff',
+                                    }}
+                                />
+                            </Row>
+                            <Row>
+                                <FirebaeLogo
+                                    style={{
+                                        width: '6rem',
+                                        paddingBottom: '1rem',
+                                        fill: '#fff',
+                                    }}
+                                />
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col md="6" style={{ padding: '2rem' }}>
+                    <Carousel slides={slides} />
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
