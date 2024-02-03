4c982cfbd5a3ff66bcbcd0510e11a0e47232093c
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-02-02 20:13:06 -0700

    Redploying

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,12 +8,28 @@ import {
 import Carousel from 'react-spring-3d-carousel';
 import Project from './Project';
 
+import {
+    faChevronLeft,
+    faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const projectsData = [EmailManagerData, PaxxiumData];
 
 const Portfolio = () => {
     const theme = useContext(ThemeContext);
 
     const [slideIndex, setSlideIndex] = useState(0);
+
+    const goToNextSlide = () => {
+        const newIndex = slideIndex === slides.length - 1 ? 0 : slideIndex + 1;
+        setSlideIndex(newIndex);
+    };
+
+    const goToPreviousSlide = () => {
+        const newIndex = slideIndex === 0 ? slides.length - 1 : slideIndex - 1;
+        setSlideIndex(newIndex);
+    };
 
     const slides = projectsData.map((data, index) => ({
         key: index,
@@ -37,14 +53,28 @@ const Portfolio = () => {
             }}
             fluid
         >
-            <Row>
-                <Col>
+            <Row
+                className="align-items-center"
+                style={{ width: '100%', height: '100%' }}
+            >
+                {/* Previous Button Column */}
+                <Col xs="1" className="d-flex justify-content-center">
+                    <FontAwesomeIcon icon={faChevronLeft} onClick={goToPreviousSlide} />
+                </Col>
+
+                {/* Carousel Column */}
+                <Col xs="10">
                     <Carousel
                         slides={slides}
                         goToSlide={slideIndex}
                         animationConfig={{ tension: 170, friction: 26 }}
-                        style={{ width: '100vw', height: '100vh' }}
+                        style={{ width: '100%', height: '100%' }}
                     />
+                </Col>
+
+                {/* Next Button Column */}
+                <Col xs="1" className="d-flex justify-content-center">
+                    <FontAwesomeIcon icon={faChevronRight} onClick={goToNextSlide} />
                 </Col>
             </Row>
         </Container>
