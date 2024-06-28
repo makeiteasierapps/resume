0e366f0a09df62f1c3957c752ff4f1d562ed6059
Shaun Offenbacher <130625311+makeiteasierapps@users.noreply.github.com>
2024-06-27 21:10:03 -0600

    ignored .env

--- src/components/chat/useChatManager.js
+++ src/components/chat/useChatManager.js
@@ -1,175 +1,168 @@
-import { useState, useRef, useContext, useEffect, useCallback } from 'react';
-import { SnackbarContext } from '../../contexts/SnackbarContext';
-import { processToken } from './utils/processToken';
+import { useState, useRef, useContext, useEffect, useCallback } from "react";
+import { SnackbarContext } from "../../contexts/SnackbarContext";
+import { processToken } from "./utils/processToken";
 
 export const useChatManager = () => {
-    const { showSnackbar } = useContext(SnackbarContext);
-    const [isLoading, setIsLoading] = useState(false);
-    const [messages, setMessages] = useState({});
-    const [insideCodeBlock, setInsideCodeBlock] = useState(false);
-    const ignoreNextTokenRef = useRef(false);
-    const languageRef = useRef(null);
-    const [isChatOpen, setIsChatOpen] = useState(false);
+  const { showSnackbar } = useContext(SnackbarContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState({});
+  const [insideCodeBlock, setInsideCodeBlock] = useState(false);
+  const ignoreNextTokenRef = useRef(false);
+  const languageRef = useRef(null);
+  const [isChatOpen, setIsChatOpen] = useState(false);
 
-    const BACKEND_URL_PROD = process.env.REACT_APP_BACKEND_URL_PROD;
-    const API_KEY = process.env.REACT_APP_API_KEY;
+  const BACKEND_URL_PROD = process.env.REACT_APP_BACKEND_URL_PROD;
+  const API_KEY = process.env.REACT_APP_API_KEY;
 
-    useEffect(() => {
-        setIsLoading(true);
-    }, []);
+  useEffect(() => {
+    setIsLoading(true);
+  }, []);
 
-    const getChatHistory = (chatId) => {
-        return messages[chatId] || [];
+  const getChatHistory = (chatId) => {
+    return messages[chatId] || [];
+  };
+
+  const getMessages = useCallback(() => {
+    try {
+      const localMessages = JSON.parse(localStorage.getItem("messages"));
+      if (localMessages) {
+        setMessages(localMessages);
+      }
+    } catch (error) {
+      console.error(error);
+      showSnackbar(`Network or fetch error: ${error.message}`, "error");
+    }
+  }, [showSnackbar]);
+
+  const addMessage = async (chatId, newMessage) => {
+    setMessages((prevMessageParts) => {
+      const updatedMessages = {
+        ...prevMessageParts,
+        [chatId]: [...(prevMessageParts[chatId] || []), newMessage],
+      };
+      localStorage.setItem("messages", JSON.stringify(updatedMessages));
+      return updatedMessages;
+    });
+  };
+
+  const sendMessage = async (chatId, input) => {
+    const userMessage = {
+      content: input,
+      message_from: "user",
+      time_stamp: new Date().toISOString(),
+      type: "database",
     };
+    addMessage(chatId, userMessage);
 
-    const getMessages = useCallback(() => {
-        try {
-            const localMessages = JSON.parse(localStorage.getItem('messages'));
-            if (localMessages) {
-                setMessages(localMessages);
-            }
-        } catch (error) {
-            console.error(error);
-            showSnackbar(`Network or fetch error: ${error.message}`, 'error');
-        }
-    }, [showSnackbar]);
+    const chatHistory = getChatHistory(chatId);
 
-    const addMessage = async (chatId, newMessage) => {
-        setMessages((prevMessageParts) => {
-            const updatedMessages = {
-                ...prevMessageParts,
-                [chatId]: [...(prevMessageParts[chatId] || []), newMessage],
-            };
-            localStorage.setItem('messages', JSON.stringify(updatedMessages));
-            return updatedMessages;
-        });
-    };
+    try {
+      const response = await sendUserMessage(chatId, userMessage, chatHistory);
+      await handleStreamingResponse(response, chatId);
+    } catch (error) {
+      console.error(error);
+      showSnackbar(`Network or fetch error: ${error.message}`, "error");
+    }
+  };
 
-    const sendMessage = async (chatId, input) => {
-        const userMessage = {
-            content: input,
-            message_from: 'user',
-            time_stamp: new Date().toISOString(),
-            type: 'database',
-        };
-        addMessage(chatId, userMessage);
+  const sendUserMessage = async (chatId, userMessage, chatHistory) => {
+    console.log(messages);
+    const response = await fetch(`http://127.0.0.1:8000/messages`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-Key": API_KEY,
+      },
+      body: JSON.stringify({
+        chatId: chatId,
+        projectId: "666e139da8a159c87447c8c1",
+        dbName: "paxxium",
+        chatHistory: chatHistory,
+        userMessage: userMessage,
+        saveToDb: false,
+        createVectorPipeline: true,
+      }),
+    });
 
-        const chatHistory = getChatHistory(chatId);
+    if (!response.ok) {
+      throw new Error("Failed to send message");
+    }
+    return response;
+  };
 
-        try {
-            const response = await sendUserMessage(
-                chatId,
-                userMessage,
-                chatHistory
-            );
-            await handleStreamingResponse(response, chatId);
-        } catch (error) {
-            console.error(error);
-            showSnackbar(`Network or fetch error: ${error.message}`, 'error');
-        }
-    };
+  const handleStreamingResponse = async (response, chatId) => {
+    const reader = response.body.getReader();
+    let completeMessage = "";
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      const decodedValue = new TextDecoder("utf-8").decode(value);
+      console.log(decodedValue);
+      const jsonChunks = decodedValue
+        .split("\n")
+        .filter((line) => line.trim() !== "");
 
-    const sendUserMessage = async (chatId, userMessage, chatHistory) => {
-        console.log(messages);
-        const response = await fetch(
-            `http://127.0.0.1:30000/chatMobile/messages`,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-API-Key': API_KEY,
-                },
-                body: JSON.stringify({
-                    chatId: chatId,
-                    projectId: '666e139da8a159c87447c8c1',
-                    dbName: 'paxxium',
-                    chatHistory: chatHistory,
-                    userMessage: userMessage,
-                    saveToDb: false,
-                    createVectorPipeline: true,
-                }),
-            }
+      const messages = jsonChunks.map((chunk) => {
+        const messageObj = JSON.parse(chunk);
+        processToken(
+          messageObj,
+          setInsideCodeBlock,
+          insideCodeBlock,
+          setMessages,
+          chatId,
+          ignoreNextTokenRef,
+          languageRef
         );
+        return messageObj.content;
+      });
+      completeMessage += messages.join("");
+    }
+    console.log(completeMessage);
+    await updateMessagesState(chatId, completeMessage);
+  };
 
-        if (!response.ok) {
-            throw new Error('Failed to send message');
-        }
-        return response;
-    };
+  const updateMessagesState = async (chatId, completeMessage) => {
+    setMessages((prevMessages) => {
+      const updatedMessages = [
+        ...(prevMessages[chatId] || []).slice(0, -1),
+        {
+          content: completeMessage,
+          message_from: "agent",
+          type: "database",
+        },
+      ];
 
-    const handleStreamingResponse = async (response, chatId) => {
-        const reader = response.body.getReader();
-        let completeMessage = '';
-        while (true) {
-            const { done, value } = await reader.read();
-            if (done) {
-                break;
-            }
-            const decodedValue = new TextDecoder('utf-8').decode(value);
-            console.log(decodedValue);
-            const jsonChunks = decodedValue
-                .split('\n')
-                .filter((line) => line.trim() !== '');
+      const newMessagesState = {
+        ...prevMessages,
+        [chatId]: updatedMessages,
+      };
 
-            const messages = jsonChunks.map((chunk) => {
-                const messageObj = JSON.parse(chunk);
-                processToken(
-                    messageObj,
-                    setInsideCodeBlock,
-                    insideCodeBlock,
-                    setMessages,
-                    chatId,
-                    ignoreNextTokenRef,
-                    languageRef
-                );
-                return messageObj.content;
-            });
-            completeMessage += messages.join('');
-        }
-        console.log(completeMessage);
-        await updateMessagesState(chatId, completeMessage);
-    };
+      localStorage.setItem("messages", JSON.stringify(newMessagesState));
+      return newMessagesState;
+    });
+  };
 
-    const updateMessagesState = async (chatId, completeMessage) => {
-        setMessages((prevMessages) => {
-            const updatedMessages = [
-                ...(prevMessages[chatId] || []).slice(0, -1),
-                {
-                    content: completeMessage,
-                    message_from: 'agent',
-                    type: 'database',
-                },
-            ];
+  const clearChat = async () => {
+    try {
+      // Update the messages state for the UI to reflect the cleared messages
+      setMessages({});
+      localStorage.removeItem("messages");
+    } catch (error) {
+      console.error(error);
+      showSnackbar(`Network or fetch error: ${error.message}`, "error");
+    }
+  };
 
-            const newMessagesState = {
-                ...prevMessages,
-                [chatId]: updatedMessages,
-            };
-
-            localStorage.setItem('messages', JSON.stringify(newMessagesState));
-            return newMessagesState;
-        });
-    };
-
-    const clearChat = async () => {
-        try {
-            // Update the messages state for the UI to reflect the cleared messages
-            setMessages({});
-            localStorage.removeItem('messages');
-        } catch (error) {
-            console.error(error);
-            showSnackbar(`Network or fetch error: ${error.message}`, 'error');
-        }
-    };
-
-    return {
-        isChatOpen,
-        setIsChatOpen,
-        messages,
-        isLoading,
-        addMessage,
-        sendMessage,
-        getMessages,
-        clearChat,
-    };
+  return {
+    isChatOpen,
+    setIsChatOpen,
+    messages,
+    isLoading,
+    addMessage,
+    sendMessage,
+    getMessages,
+    clearChat,
+  };
 };
